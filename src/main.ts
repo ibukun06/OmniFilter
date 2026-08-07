aa68be6ee115f2d98bc50c7885a6e5fead340f30
Ibukunoluwa Oluwafemi <68595066+ibukun06@users.noreply.github.com>
2026-08-07 14:19:29 +0100

    Initialize NestJS application with AuthModule

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AuthModule } from './modules/auth/auth.module';
+
+async function bootstrap() {
+
+  const app =
+    await NestFactory.create(AuthModule);
+
+  app.enableCors();
+
+  await app.listen(3001);
+
+}
+
+bootstrap();
