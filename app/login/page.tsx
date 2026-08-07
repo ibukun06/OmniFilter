e1a4bd45f8ee642ef321521ba8d06bcbc2d25a78
Ibukunoluwa Oluwafemi <68595066+ibukun06@users.noreply.github.com>
2026-08-07 14:20:50 +0100

    Create LoginPage component for user login

---
+++ app/login/page.tsx
@@ -0,0 +1,26 @@
+export default function LoginPage() {
+
+ return (
+
+  <main>
+
+   <h1>Login</h1>
+
+   <input
+     placeholder="Email"
+   />
+
+   <input
+     type="password"
+     placeholder="Password"
+   />
+
+   <button>
+      Sign In
+   </button>
+
+  </main>
+
+ )
+
+}
